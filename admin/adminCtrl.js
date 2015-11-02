097cccc08f1fb3def51fbf14085a876076609fe8
kamlesh <kamlesh.205110047@gmail.com>
2015-11-03 01:22:39 +0530

    final commit

--- admin/adminCtrl.js
+++ admin/adminCtrl.js
@@ -3,9 +3,16 @@ angular.module('foldKartAdmin',[]).controller('adminController', function($scope
 	/* Open the admin login modal */
 	$('#adminLoginModal').modal('show');
 
+	$scope.getAllProducts = function() {
+        adminService.getAllProducts().then(function(data){
+			$scope.allProducts = adminService.addCategoryIntoProducts(data,$scope.allCategory)
+		})
+	}
+
 	$scope.getAllCategory = function() {
 		adminService.getAllCategory().then(function(data){
 			$scope.allCategory = data;
+			$scope.getAllProducts();
 		})
 	}
 
@@ -36,7 +43,10 @@ angular.module('foldKartAdmin',[]).controller('adminController', function($scope
 			$scope.product.quantity  = 1;
 			adminService.addProduct($scope.product,$scope.admin).then(function(data){
 				$scope.product = {};
+				$scope.allProducts.push(data)
+				$scope.allProducts = adminService.addCategoryIntoProducts($scope.allProducts,$scope.allCategory)
 				alert("Successfully Added !");
+				$('#addProductModal').modal('hide');
 			})
 		}else {
            alert("Please select Category");
@@ -48,6 +58,31 @@ angular.module('foldKartAdmin',[]).controller('adminController', function($scope
 		location.reload(); 
 	}
 
+	$scope.deleteProduct = function(product_id) {
+		adminService.deleteProduct(product_id).then(function(data){
+			$scope.allProducts = _.reject($scope.allProducts, function(product) { return product.id === product_id; });
+		})
+	}
 
+    $scope.openAddCategoryModal = function() {
+		$('#addProductModal').modal('hide');
+		$('#addCategoryModal').modal('show');
+	}
+
+	$scope.updateProductModal = function(product_object) {
+		$scope.productForUpdate = jQuery.extend(true, {}, product_object);
+	}
+
+	$scope.updateProduct = function() {
+		var tempCategoryName = $scope.productForUpdate.category;
+		$scope.productForUpdate.category = adminService.addCategoryIdForProduct($scope.productForUpdate.category,$scope.allCategory);
+		adminService.updateProduct($scope.productForUpdate).then(function(data){
+			$scope.allProducts = _.reject($scope.allProducts, function(product) { return product.id === data.id; });
+			$scope.productForUpdate.category = tempCategoryName;
+			$scope.allProducts.unshift($scope.productForUpdate);
+			alert("Successfully Updated !");
+			$('#updateProductModal').modal('hide');
+		})
+	}
 
 });
--- admin/adminService.js
+++ admin/adminService.js
@@ -1,5 +1,39 @@
 angular.module('foldKartAdmin').service('adminService', function($http,$q) {
 
+    this.addCategoryIntoProducts = function(allProducts,allCategory) {
+       for(var i=0 ; i<allProducts.length ; i++) {
+       	var tempId = allProducts[i].category;
+       	for(var j=0 ; j<allCategory.length ; j++) {
+       		if(allCategory[j].id === tempId) {
+       			allProducts[i].category = allCategory[j].category;
+       		}
+       	}
+       }
+       return allProducts;
+	}
+
+	this.addCategoryIdForProduct = function(products_category_name,allCategory) {
+       	for(var j=0 ; j<allCategory.length ; j++) {
+       		if(allCategory[j].category === products_category_name) {
+       			return allCategory[j].id;
+       		}
+       	}
+	}
+
+    this.getAllProducts = function() {
+    	var deferred = $q.defer();
+		$http({ method: 'GET', url: 'https://my-selfie-tech-login.apispark.net/v5/productses' })
+		.success(function (data, status, headers, config)
+		{
+			deferred.resolve(data);
+		})
+		.error(function (data, status, headers, config)
+		{
+			deferred.reject(data, status, headers, config);
+		})
+		return deferred.promise;
+    }
+    
 	var findUser = function(data,admin) {
 		return _.find(data, function(user) { 
 			return ( user.name === admin.name && user.password === admin.password )
@@ -69,5 +103,35 @@ angular.module('foldKartAdmin').service('adminService', function($http,$q) {
 		})
 		return deferred.promise;
 	}
+
+
+	this.deleteProduct = function(product_id) {
+		var deferred = $q.defer();
+		$http({ method: 'DELETE', url: 'https://my-selfie-tech-login.apispark.net/v5/productses/'+ product_id })
+		.success(function (data, status, headers, config)
+		{
+			deferred.resolve(data);
+		})
+		.error(function (data, status, headers, config)
+		{
+			deferred.reject(data);
+		})
+		return deferred.promise;
+	}
+
+	this.updateProduct = function(product) {
+        var deferred = $q.defer();
+		$http({ method: 'PUT', url: 'https://my-selfie-tech-login.apispark.net/v5/productses/'+ product.id , data : product })
+		.success(function (data, status, headers, config)
+		{
+			deferred.resolve(data);
+		})
+		.error(function (data, status, headers, config)
+		{
+			deferred.reject(data);
+		})
+		return deferred.promise;
+	}
+
 
 });
